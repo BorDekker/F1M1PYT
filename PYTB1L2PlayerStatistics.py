657a723080d656cf775c93854f99c2ffdcdf21e6
Bor Dekker <32562@ma-web.nl>
2021-09-23 13:26:02 +0200

    (PlayerStatistic)

---
+++ PYTB1L2PlayerStatistics.py
@@ -0,0 +1,23 @@
+Name = "Bor DEKKER"
+Health = 40
+Studeert = True
+Attunement = 20
+Endurance = 40
+Strenght = 60
+Dexterity = 40
+Intelligence = 26
+Faith = 24
+Luck = 9000
+Skills = 20
+
+print("Name : "+ Name)
+print("Health :"+ str(Health))
+print("Studeert :"+str(Studeert))
+print("Attunement:"+str(Attunement))
+print("Endurance:"+str(Endurance))
+print("Strenght:"+str(Strenght))
+print("Dexterity:"+str(Dexterity))
+print("Intelligence:"+str(Intelligence))
+print("Faith:"+str(Faith))
+print("Luck:"+str(Luck))
+print("Skills:"+str(Skills))
